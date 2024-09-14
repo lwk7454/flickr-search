ac890f01f87b8b56315af881fdb8ee636fb823ad
Wayne Liao <lwk7454@gmail.com>
2024-09-14 17:19:37 +1000

    chore: update readme; minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Flickr Search
+
+Visit: https://flickr-search-phi.vercel.app/
+
+## Prerequisites
+- Bun
+
+## Getting Started
+Install dependencies:
+```bash
+bun install
+```
+
+Run the development server:
+```bash
+bun run dev
+```
+
+Run tests:
+```bash
+bun run test
+```
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 w-full">
+    <main className="flex min-h-screen flex-col items-center py-12 px-4 md:py-24 md:px-12 w-full">
       <h1 className="text-4xl font-extrabold text-center text-teal-500 mb-8">Flickr Search</h1>
       <SearchResults />
     </main>
